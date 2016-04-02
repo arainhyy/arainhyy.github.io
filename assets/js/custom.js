d6650ef2636837ac200833feca2d7908bd8d062a
Yayun Huang <arainhyy@gmail.com>
2016-04-01 21:00:18 -0400

    set up website

---
+++ assets/js/custom.js
@@ -0,0 +1,88 @@
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            /*====================================
+             CUSTOM LINKS SCROLLING FUNCTION 
+            ======================================*/
+
+            $('a[href*=#]').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+               && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length && $target
+                    || $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        var targetOffset = $target.offset().top;
+                        $('html,body')
+                        .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                        return false;
+                    }
+                }
+            });
+            /*====================================
+            VAGAS SLIDESHOW SCRIPTS
+            ======================================*/
+            $(function () {
+                $.vegas('slideshow', {
+                    backgrounds: [
+                      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
+                      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
+                       { src: 'assets/img/3.jpg', fade: 1000, delay: 9000 }, 
+                     
+                    ]
+                })('overlay', {
+                    /** SLIDESHOW OVERLAY IMAGE **/
+                    src: 'assets/plugins/vegas/overlays/15.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+                });
+
+            });
+
+            /*====================================
+                NAV SCRIPTS
+            ======================================*/
+            $(window).bind('scroll', function () {
+                var navHeight = $(window).height() -50;
+                if ($(window).scrollTop() > navHeight) {
+                    $('nav').addClass('fixed');
+                }
+                else {
+                    $('nav').removeClass('fixed');
+                }
+            });
+
+            /*====================================
+               WRITE YOUR SCRIPTS BELOW 
+           ======================================*/
+          
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+
